2600af22c7f98d770297fe881695bb0b8e64357f
VICOrepo <viral.jain.2019@ecajmer.ac.in>
2023-04-18 10:44:40 +0530

    finalupdate

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,7 +6,12 @@ router.route('/').get((req, res) => {
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
- 
+
+router.route('/:id').delete((req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(() => res.json('User deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 router.route('/add').post((req, res) => {
   const username = req.body.username;
 
